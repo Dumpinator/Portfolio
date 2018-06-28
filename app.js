5b623a4873f25f160953fe7a111eceabb15c945e
Dump <jdeboisvilliers@becoast.fr>
2018-06-29 00:13:52 +0200

    add shadow behance

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 class Portfolio {
 
 	constructor (selector) {
+		this.activeContent = null
+		this.activeItem = null
 		this.container = document.querySelector(selector)
 		if (this.container === null) {
 			throw new Error(`L'élément ${selector} n'existe pas !`)
@@ -16,19 +18,58 @@ class Portfolio {
 	}
 
  	show (child) {
-		let content = child.querySelector('.js-body').cloneNode(true)
-		child.after(content)
-		this.slideDown(content)
+		let offset = 0
+
+		if (this.activeContent !== null) {
+			this.slideUp(this.activeContent)
+			if (this.activeContent.offsetTop < child.offsetTop) {
+				offset = this.activeContent.offsetHeight
+			}
+		}
+		if (this.activeItem === child) {
+			this.activeContent = null
+			this.activeItem = null
+		} else {
+			let content = child.querySelector('.js-body').cloneNode(true)
+
+			child.after(content)
+			this.slideDown(content)
+			this.scrollTo(child, offset)
+			this.activeContent = content
+			this.activeItem = child
+		}
 	}
 
 	slideDown (element) {
 		let height = element.offsetHeight
+
 		element.style.height = '0px'
 		element.style.transitionDuration = '.5s'
 		element.offsetHeight //force le repeat
 		element.style.height = height + 'px'
+		window.setTimeout(function () {
+			element.style.height = null
+		}, 500)
 	}
 
+	slideUp (element) {
+		let height = element.offsetHeight
+
+		element.style.height = height + 'px'
+		element.offsetHeight //force le repeat
+		element.style.height = '0px'
+		window.setTimeout(function () {
+			element.parentNode.removeChild(element)
+		}, 500)
+	}
+
+	scrollTo (element, offset = 0) {
+		window.scrollTo({
+			behavior: 'smooth',
+			left: 0,
+			top: element.offsetTop - offset
+		})
+	}
 }
 
 new Portfolio ('#js-portfolio')
