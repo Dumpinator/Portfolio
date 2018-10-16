ab23fdb691f57709734bcfd4b25115b0846ddec3
Dumpinator <jdeboisvilliers@gmail.com>
2018-10-16 12:26:59 +0200

    add project 2
    + img 2

--- js/data.js
+++ js/data.js
@@ -10,4 +10,17 @@ export default [{
   "paraDescription": "Site internet réaliser pour le lancement du MVP pour le concept d'application de Becoast, un One Page responsive et efficace qui permettait d'avoir un CTA pour une carte météo dynamique.",
   "linkLanguage": ["HTML","CSS","JS"],
   "linkDescription": "https://becoast.fr"
+},
+{
+  "src": "img/p2.jpg",
+  "date": "2018",
+  "name": "StarWarsAcademy",
+  "title": "En cours de réalisation...",
+  "generateTag": {
+    "link": "https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API",
+    "name": "Fetch API" },
+  "titleDescription": "Manipulation de data depuis une URL",
+  "paraDescription": "Récupération de données depuis une URL avec la méthode fetch et injection de ces données dans une template string ",
+  "linkLanguage": ["HTML","CSS","JS"],
+  "linkDescription": "https://dumpinator.github.io/StarWarsAcademy/"
 }]
