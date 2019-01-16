74002fdabc73d53b5cff271b7b433fbd0e5f6d99
Dumpinator <jdeboisvilliers@gmail.com>
2019-01-16 17:17:02 +0100

    add project 3
    + about content
    + css

--- js/data.js
+++ js/data.js
@@ -15,14 +15,28 @@ export default [{
 {
   "src": "img/p2.jpg",
   "date": "2018",
-  "name": "StarWarsAcademy",
-  "title": "En cours de réalisation...",
+  "name": "Star Wars Academy",
+  "title": "Glossaire des personnages",
   "generateTag": {
-    "name": ["Fetch API","JS"],
-    "link": ["https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API","https://developer.mozilla.org/fr/docs/Web/JavaScript"],
-    "cssColor": ["js","js"] },
+    "name": ["Fetch API", "Github Pages"],
+    "link": ["https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API","https://pages.github.com/"],
+    "cssColor": ["js","github"] },
   "titleDescription": "Manipulation de data depuis une URL",
   "paraDescription": "- Récupération de données depuis une URL avec la méthode fetch<br> - injection de ces données dans le DOM avec une template string<br> - Mise en place d'un filtre par espèces : [ Human, Droid, Other et All ]",
   "linkLanguage": ["HTML","CSS","JS"],
   "linkDescription": "https://dumpinator.github.io/StarWarsAcademy/"
+},
+{
+  "src": "img/p3.jpg",
+  "date": "2019",
+  "name": "Golden Wall",
+  "title": "Mur à la Twitter",
+  "generateTag": {
+    "name": ["MeteorJS","BlazeJS","Bootstrap","MongoDB","Heroku"],
+    "link": ["https://www.meteor.com/","http://blazejs.org/","https://getbootstrap.com","https://docs.mongodb.com/","https://dashboard.heroku.com/"],
+    "cssColor": ["meteor","blazejs","bootstrap","mongodb","heroku"] },
+  "titleDescription": "Exercice de CRUD",
+  "paraDescription": "- Create, Read, Update, Delete <br> - Prise en main de MeteorJS <br> - Templating avec BlazeJS <br> - Gestion utilisateur sur MongoDB",
+  "linkLanguage": ["HTML","CSS","JS"],
+  "linkDescription": "https://golden-wall.herokuapp.com/"
 }]
